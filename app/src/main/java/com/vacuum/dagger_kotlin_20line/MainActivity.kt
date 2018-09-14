dbe181bfe427b2311395fbeaae9fd37798e8a462
Mohamed Ebrahim <ebrahimm131@gmail.com>
2018-09-14 06:47:11 +0200

    first commit

---
+++ app/src/main/java/com/vacuum/dagger_kotlin_20line/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.vacuum.dagger_kotlin_20line
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import dagger.Component
+import kotlinx.android.synthetic.main.activity_main.*
+import javax.inject.Inject
+
+class MainActivity : AppCompatActivity() {
+
+    @Inject lateinit var test :Info
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //DaggerInfoComponant.create().inject(this)
+        text.setText(test.info)
+    }
+}
+
+class Info @Inject constructor(){
+    val info = "Hello porn"
+}
+
+
+@Component
+interface InfoComponant{
+    fun inject(main:MainActivity)
+}
